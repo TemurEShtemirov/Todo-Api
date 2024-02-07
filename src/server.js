25d63211e7a895df1b6e93e8744f65bb7ec06b90
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-02-07 23:18:16 +0500

    created server and connected to db

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+async function bootstrap() {
+  const app = express();
+  // port
+  const port = process.env.PORT || 4852;
+  // MongoDB Url
+  const mongo_url = process.env.DBURL || "mongodb://localhost:27017/todos";
+
+  mongoose
+    .connect(mongo_url)
+    .then(() => console.log(`SERVER CONNECTED TO MONGODB SUCCESSFULY`))
+    .catch(() => console.log(`MONGODB ERROR: ${err}`));
+
+    app.listen(port,()=>console.log(`Server listening on ${port}`))
+}
+
+bootstrap()
